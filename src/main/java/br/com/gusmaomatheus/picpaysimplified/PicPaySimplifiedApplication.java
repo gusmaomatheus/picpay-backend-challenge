01393e905196d46c7344b45fc10cbd241f9f021c
Matheus Gusmão <mgusmao412@gmail.com>
2024-01-18 00:07:23 -0300

    feat: create project base

---
+++ src/main/java/br/com/gusmaomatheus/picpaysimplified/PicPaySimplifiedApplication.java
@@ -0,0 +1,13 @@
+package br.com.gusmaomatheus.picpaysimplified;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicPaySimplifiedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicPaySimplifiedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/gusmaomatheus/picpaysimplified/PicpaySimpliedApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gusmaomatheus.picpaysimplified;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicpaySimpliedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
